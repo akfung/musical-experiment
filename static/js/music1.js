01fa13585aca834cc56024c9f8bc5d9d24b40ba8
Amos Fung <amosfung@amoss-mbp.home>
2020-06-24 13:05:41 -0700

    initial framework with p5

--- static/js/music1.js
+++ static/js/music1.js
@@ -1,52 +1,87 @@
-// Drum beat
-DRUMS = {
-    notes: [
-      { pitch: 36, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-      { pitch: 38, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-      { pitch: 42, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-      { pitch: 46, quantizedStartStep: 0, quantizedEndStep: 1, isDrum: true },
-      { pitch: 42, quantizedStartStep: 2, quantizedEndStep: 3, isDrum: true },
-      { pitch: 42, quantizedStartStep: 3, quantizedEndStep: 4, isDrum: true },
-      { pitch: 42, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
-      { pitch: 50, quantizedStartStep: 4, quantizedEndStep: 5, isDrum: true },
-      { pitch: 36, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-      { pitch: 38, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-      { pitch: 42, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-      { pitch: 45, quantizedStartStep: 6, quantizedEndStep: 7, isDrum: true },
-      { pitch: 36, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
-      { pitch: 42, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
-      { pitch: 46, quantizedStartStep: 8, quantizedEndStep: 9, isDrum: true },
-      { pitch: 42, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true },
-      { pitch: 48, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true },
-      { pitch: 50, quantizedStartStep: 10, quantizedEndStep: 11, isDrum: true },
-    ],
-    quantizationInfo: {stepsPerQuarter: 4},
-    tempos: [{time: 0, qpm: 120}],
-    totalQuantizedSteps: 11
-  };
+//initialize vars
+let mrNoisy, playButton, stopButton, chooseNoise, setVolume, toggleOnOff, fft;
 
+function setup() {
+  //object background
+  createCanvas(400, 200);
+  //create new noise generator
+  mrNoisy = new p5.Noise();
+  //set initial amplitude
+  mrNoisy.amp(0);
 
-//create player
-player = new mm.Player();
+  //fast fourier transform for waveform visualization
+  fft = new p5.FFT();
 
-//button plays music
-function startOrStop(event, p, seq = DRUMS) {
-    if (p.isPlaying()) {
-        p.stop();
-        event.target.textContent= 'Play';
+  //play button toggles depending on if music is playing
+  toggleOnOff = createButton("play")
+    .position(10, 10)
+    .style("font-family", "courier");
+  //event listener on mouse press
+  toggleOnOff.mousePressed(() => {
+    if (mrNoisy.started) {
+      mrNoisy.stop();
+      toggleOnOff.html("play");
+    } else {
+      mrNoisy.start();
+      toggleOnOff.html("stop");
     }
-    else {
-        p.start(seq).then(() => {
-            const btns = document.querySelectorAll('.controls > button');
-            for (let btn of btns) {
-                btn.textContent = 'Play';
-            }
-        })
-        event.target.textContent = 'Stop';
-    }
+  });
+
+  //noise selection dropdown
+  chooseNoise = createSelect();
+  chooseNoise.position(60, 10); //set location
+  chooseNoise.option("white");
+  chooseNoise.option("pink");
+  chooseNoise.option("brown");
+  //listener to change noise generator with setType method
+  chooseNoise.changed(() => {
+    mrNoisy.setType(chooseNoise.value());
+    fill(chooseNoise.value()); //set waveform fill to noise color
+  });
+
+  //create volume slider
+  setVolume = createSlider(-60, 0, -60, 1);
+  setVolume.position(130, 10);
+  // use .input listener for changing volume
+  setVolume.input(() => {
+    //volume slider is logarithmic
+    mrNoisy.amp(pow(10, setVolume.value() / 20), 0.01);
+  });
+
+  //set the color of the waveform to white
+  fill("white");
+  noStroke();
 }
 
-// play the song
-player.start(DRUMS);
-player.stop();
+//draw visualizer
+function draw() {
+  background(80);
+  let spectrum = fft.analyze(); //assign fft array output to spectrum variable
 
+  //beginShape and endShape use vertex defined points
+  beginShape();
+  vertex(0, height);
+  //loop through all frequencies
+  for (let i = 0; i < spectrum.length - 1; i++) {
+    vertex(
+      map(log(i), 0, log(spectrum.length), 0, width),
+      map(spectrum[i], 0, 255, height, 0)
+    );
+  }
+  vertex(width, height);
+  endShape();
+}
+
+//resume audio for chrome
+function touchStarted() {
+  if (getAudioContext().state !== "running") {
+    getAudioContext().resume();
+  }
+}
+
+//listener to get audio started
+document.querySelector(".play-button").addEventListener("click", function () {
+  context.resume().then(() => {
+    console.log("Playback resumed successfully");
+  });
+});
